26a3d11f992dc9b8fe2d60fb0fb71303c8bbaf08
Kevin <carrerkokito@gmail.com>
2023-08-20 14:56:01 -0400

    Add serializers.py

---
+++ BackEnd/back/base/serializers.py
@@ -0,0 +1,29 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
+
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+
+    def validate(self, attrs):
+        email_exists = User.objects.filter(email=attrs['email']).exists()
+
+        if email_exists:
+            raise ValidationError('Email already in use')
+
+        return super().validate(attrs)
+
+    def create(self, validated_data):
+        user = User.objects.create(
+            username=validated_data['username'],
+            email=validated_data['email'],
+        )
+
+        user.set_password(validated_data['password'])
+        user.save()
+
+        return user
