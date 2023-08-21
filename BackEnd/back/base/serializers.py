583db9dd13f6b12484a28ab23b1d3c541dfd5ee3
Kevin <carrerkokito@gmail.com>
2023-08-21 02:33:37 -0400

    Remove sensible data from the public api

--- BackEnd/back/base/serializers.py
+++ BackEnd/back/base/serializers.py
@@ -7,23 +7,23 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password']
+        fields = ['id', 'username']
 
-    def validate(self, attrs):
-        email_exists = User.objects.filter(email=attrs['email']).exists()
-
-        if email_exists:
-            raise ValidationError('Email already in use')
-
-        return super().validate(attrs)
-
-    def create(self, validated_data):
-        user = User.objects.create(
-            username=validated_data['username'],
-            email=validated_data['email'],
-        )
-
-        user.set_password(validated_data['password'])
-        user.save()
-
-        return user
+    # def validate(self, attrs):
+    #     email_exists = User.objects.filter(email=attrs['email']).exists()
+    #
+    #     if email_exists:
+    #         raise ValidationError('Email already in use')
+    #
+    #     return super().validate(attrs)
+    #
+    # def create(self, validated_data):
+    #     user = User.objects.create(
+    #         username=validated_data['username'],
+    #         email=validated_data['email'],
+    #     )
+    #
+    #     user.set_password(validated_data['password'])
+    #     user.save()
+    #
+    #     return user
