92e4a7d8301cc220328cf5eaf8ded34920c4fa04
frankxhunter <frankxhunter@gmail.com>
2023-08-22 11:08:34 -0400

    backend defectuoso eliminado, y actualizado nombre de las carpetas a nombres mas comodos

--- BackEnd/back/README.md
+++
@@ -1,2 +0,0 @@
-En http://127.0.0.1:8000/api/ vas a encontrar todas las rutas con sus
-metodos permitidos
--- BackEnd/back/chats/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-from rest_framework.authtoken.models import Token
-
-
-# Create your views here.
